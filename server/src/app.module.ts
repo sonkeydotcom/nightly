fe12f467b11b128331dc3eaaf76c599659efe9e3
sonkey <annagu.kennedy@gmail.com>
2024-06-08 14:22:51 +0000

    Jun 8, 2024, 4:22 PM

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BookmartModule } from './bookmart/bookmart.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, BookmartModule],
 })
 export class AppModule {}
