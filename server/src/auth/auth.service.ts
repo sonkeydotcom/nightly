50ea48edaecc61d6979ce293bf6e957483aa75c7
sonkey <annagu.kennedy@gmail.com>
2024-06-09 01:02:10 +0200

    User signup

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { AuthDto } from '../dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
@@ -11,23 +12,38 @@ export class AuthService {
     const hash = await argon.hash(dto.password);
 
     // save the new user in the db
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-      select: {
-        id: true,
-        email: true,
-        createdAt: true,
-      },
-    });
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
 
-    // return the saved user
-    return user;
+      delete user.hash;
+
+      // return the saved user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+      throw error;
+    }
   }
 
-  signin() {
+  async signin(dto: AuthDto) {
+    // find the user by email
+
+    // if  the user doenot exit throw error
+
+    // comapre password
+
+    // if password incorrect  throw an error
+
+    // send back the user
     return {
       access_token: 'access_token',
     };
