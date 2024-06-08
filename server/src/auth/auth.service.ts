67cd1c24f668b50eb31e39eb58ae166707248e7b
sonkey <annagu.kennedy@gmail.com>
2024-06-08 19:20:52 +0200

    User signup

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,17 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { AuthDto } from '../dto';
 import { PrismaService } from 'src/prisma/prisma.service';
-
+import * as argon from 'argon2';
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  signin() {
-    return {
-      access_token: 'access_token',
-    };
+  async signup(dto: AuthDto) {
+    // generate the password
+    const hash = await argon.hash(dto.password);
+
+    // save the new user in the db
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+      select: {
+        id: true,
+        email: true,
+        createdAt: true,
+      },
+    });
+
+    // return the saved user
+    return user;
   }
 
-  signup() {
+  signin() {
     return {
       access_token: 'access_token',
     };
